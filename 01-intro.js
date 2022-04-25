4a701b63197d7a116c1217eee286b5a5ae63b882
Gonzo <gonzaloariaslucini@gmail.com>
2022-04-25 10:58:02 -0300

    creado js 2

--- 01-intro.js
+++ 01-intro.js
@@ -23,9 +23,13 @@ console.log("Tu color favorito es " + color);
 const anioActual = new Date().getFullYear();
 let anioNac = prompt("¿En qué año naciste?");
 anioNac = parseInt(anioNac)
+/* Parseint hace a los elementos de cadena a elementos de número (de "2022" a 2022) */
 let edad = anioActual - anioNac;
 let altura = prompt("¿Cuáno medis?")
 altura = parseFloat(altura)
 
-console.log("Hola " + nombre + " " + apellido + " tu edad es " + edad + " años y naciste en " + anioNac + " y medis " + altura + " metros, que son " + (2 - altura) + " metros menor a 2 metros");
+console.log("Hola " + nombre + " " + apellido + " tu edad es " + edad + " años y naciste en " + anioNac + " y medis " + altura + " metros, que son " + (altura -1) + " metros mayor a 1 metro");
+
+alert(sos gay;)
+
 
---
+++ 02-condicionales.js
@@ -0,0 +1,11 @@
+console.log("2 + 2");
+console.log(2 + 2);
+console.log(2>2);
+console.log(3>2);
+console.log(2>=2)
+console.log(2!=3);
+/* != es distinto, == es igual (en valores), === es igual (en contextos), <= es menor o igual y >= es mayor o igual */
+let x = 2;
+console.log (x==4)
+console.log (x==="2")
+console.log (x== "2")
