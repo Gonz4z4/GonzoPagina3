6e59ec85ab3706aea19d6aeedccb238a8adaf37c
Gonzalo <gonzaloariaslucini@gmail.com>
2022-05-02 20:06:04 -0300

    Terminada primer tarea

--- tarea.js
+++ tarea.js
@@ -58,3 +58,65 @@ suelTot=(valAllArts +sueldoFij).toFixed(3)
 
 console.log("Con un sueldo fijo de $" + sueldoFij + " y " + cantArt + " artículos vendidos en el mes a $" + valArt + " cada uno, el sueldo total debe ser de $" + suelTot )
 */
+
+// ej 7)
+/*
+let ancho; let largo; let valorM2; let valorTot; let perimetroX3
+ancho = parseInt(prompt("Ingrese el ancho del terreno en metros:"))
+largo = parseInt(prompt("Ingrese el largo del terreno en metros:"))
+valorM2=parseInt(prompt("Ingrese el valor del terreno por metro cuadrado:"))
+valorTot= ancho*largo*valorM2
+perimetroX3= (largo+ancho)*6
+
+console.log("El valor total son $" +valorTot)
+console.log("La cantidad de metros de alambrado necesarios son "+ perimetroX3)
+*/
+
+// ej 8)
+/*
+let num1; let num2
+num1= parseInt(prompt("Ingrese un número"))
+num2= parseInt(prompt("Ingrese otro número"))
+console.log(
+    num1 + "+" + num2 + "=" + (num1+num2) +"\n"+
+    num1 + "-" + num2 + "=" + (num1-num2) +"\n"+
+    num1 + "/" + num2 + "=" + (num1/num2) +"\n"+
+    num1 + "*" + num2 + "=" + (num1*num2)
+)
+*/
+
+//ej 9)
+/*
+let num1; let num2; let numTot
+num1= parseInt(prompt("Ingrese un número entero menor a 180"))
+num2= parseInt(prompt("Ingrese otro número entero menor a 180"))
+while (num1>180 || num2>180 || (num1+num2)>180) {
+    num1=parseInt(prompt("ERROR. Ingrese un número entero menor a 180"))
+    num2= parseInt(prompt("Ingrese otro número entero menor a 180"))
+}
+numTot= 180-num1-num2
+console.log("El 3er ángulo debe ser de " + numTot+"º")
+*/
+
+//ej 10)
+let nomb1; let nomb2; let nomb3; let $1; let $2; let $3; let porc1; let porc2; let porc3; let valTot
+
+nomb1= prompt("Ingrese el nombre del primer aportante")
+$1= parseInt(prompt("Ingresa el monto del primer aportante"))
+nomb2= prompt("Ingrese el nombre del segundo aportante")
+$2= parseInt(prompt("Ingresa el monto del segundo aportante"))
+nomb3= prompt("Ingrese el nombre del tercer aportante")
+$3= parseInt(prompt("Ingresa el monto del tercer aportante"))
+valTot=$1+$2+$3
+porc1=(($1/valTot)*100).toFixed(2)
+porc2=(($2/valTot)*100).toFixed(2)
+porc3=(($3/valTot)*100).toFixed(2)
+console.log (
+    nomb1 + " aporta $" + $1 + "\n" +
+    nomb2 + " aporta $" + $2 + "\n" +
+    nomb3 + " aporta $" + $3 + "\n" +
+    "El valor total es de $" + valTot + "\n" +
+    nomb1 + " aportó un " + porc1 + "%" + "\n" +
+    nomb2 + " aportó un " + porc2 + "%" + "\n" +
+    nomb3 + " aportó un " + porc3 + "%"
+)
