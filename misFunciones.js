56d0d15ab3af757b857179e854cd1e9906ba7b63
Gonzalo <gonzaloariaslucini@gmail.com>
2022-05-02 12:03:09 -0300

    creada mi propia libreria de funciones

---
+++ misFunciones.js
@@ -0,0 +1,19 @@
+function leerEnteroEntre(mensaje, min, max){
+    let entero = parseInt(prompt("Ingrese un" +mensaje));
+    while (entero<min || entero>max){
+        entero = parseInt(prompt("Error. Ingrese un" +mensaje));
+    }
+    return entero;
+}
+
+function esPar(num){
+    return num % 2 == 0;
+}
+
+function saludar(nombre){
+    console.log("Hola "+ nombre);
+}
+
+function mostrarSaltoLinea (){
+    console.log("\n") //imprime un salto de linea
+}
