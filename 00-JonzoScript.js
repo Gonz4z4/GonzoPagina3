2536b6642fca6e93a5dc678983782bda96f6f6d0
Gonzalo <gonzaloariaslucini@gmail.com>
2022-05-01 21:59:19 -0300

    creado js 00

---
+++ 00-JonzoScript.js
@@ -0,0 +1,7 @@
+console.log("Mensaje sin clasificar")
+console.info("Mensaje clasificado como información")
+console.error("Mensaje de error")
+console.warn("Mensaje de advertencia")
+console.log("%cMensaje de color verde y grande", "color: green; font-size: x-large");
+console.log("%c mensaje", "color:blue; font-size:small")
+
